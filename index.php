cd96cc90d267bb13ba5117bf40e5a641e25af09d
MichaelBadEend <D317526@edu.curio.nl>
2026-03-11 11:55:47 +0100

    index test

--- index.php
+++ index.php
@@ -13,3 +13,46 @@
 </body>
 
 </html>
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>Taakbord</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <?php require_once 'task/header.php'; ?>
+    
+    <div class="container home">
+
+        <h1>Taken:</h1>
+
+        <?php
+        require_once 'backend/conn.php';
+        $query = "SELECT * FROM taken";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        ?>
+        <Table>
+        <tr>
+            <th>Titel</th>
+            <th>Afdeling</th>
+            <th>Beschrijving</th>
+        </tr>
+        <?php foreach($taken as $taak): ?>
+            <tr>
+                <td><?php echo $taak['titel']; ?></td>
+                <td><?php echo $taak['afdeling']; ?></td>
+                <td><?php echo $taak['beschrijving']; ?></td>
+                <td><a href="task/edit.php?id=<?php echo $taak['id']; ?>">aanpassen</a></td>
+            </tr>
+        <?php endforeach; ?>
+    </Table>
+    </div>
+
+</body>
+
+</html>
